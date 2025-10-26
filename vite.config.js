dbb509344fa53338ceca60fbdf979774143b707e
sandhanu <sandhanu12@gmail.com>
2025-10-26 17:26:45 +0530

    fix: update deploy script to use 'dist' directory and set base path in Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/hackathon-website/',
   resolve: {
     alias: {
       // Add any necessary aliases here
